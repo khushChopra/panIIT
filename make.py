b5f9172c4b9fd74fbdb0a7d2e745500168c10a98
khushChopra <khush.chopra.cse16@itbhu.ac.in>
2019-01-06 01:16:55 +0530

    Y added

--- make.py
+++ make.py
@@ -3,5 +3,9 @@ import numpy as np
 
 filename="solution.csv"
 csv = np.genfromtxt(filename, dtype=int, delimiter=",",usecols=[1])
+csv = np.delete(csv, (0), axis=0)
+import pickle
+with open("Y.obj","wb") as fp:
+	pickle.dump(csv,fp)
 print(csv)
 
